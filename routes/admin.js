e684f0bff534aefe7f2eb8ae079cbd7ea177ec98
Melo <francisco.omelo89@gmail.com>
2020-11-23 12:02:56 +0000

    admin link view

--- routes/admin.js
+++ routes/admin.js
@@ -1,8 +1,12 @@
+const router = require("express").Router();
+
 router.get("/admin/updateprofile", (req, res, next) => {});
 
 router.put("/admin/updateprofile", (req, res, next) => {});
 
-router.get("/admin/links", (req, res, next) => {});
+router.get("/admin/links", (req, res, next) => {
+  res.render("admin/links");
+});
 
 router.post("/admin/links", (req, res, next) => {});
 
@@ -11,3 +15,5 @@ router.put("/admin/links", (req, res, next) => {});
 router.get("/admin/analytics", (req, res, next) => {});
 
 router.get("/admin/appearence", (req, res, next) => {});
+
+module.exports = router;
