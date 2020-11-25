103786a13161845542ccdf60c94f4559f095f347
Marcel Niebylski <mnblski@gmail.com>
2020-11-25 07:57:50 +0000

    some basic styling so its not super ugly

--- middlewares/isLoggedIn.js
+++ middlewares/isLoggedIn.js
@@ -3,6 +3,6 @@ module.exports = (req, res, next) => {
   if (!req.session.user) {
     return res.redirect("/auth/login");
   }
-  user = req.session.user;
+  req.user = req.session.user;
   next();
 };
